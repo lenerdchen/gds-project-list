9dbc428a702bc422339e3c070b6c5c390e72d985
Leonard Chen <>
2018-09-03 19:34:59 +0800

    more styled components

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,9 +1,77 @@
 import React from "react";
 import styled from "styled-components";
 
+const Wrapper = styled.div`
+  margin: 2em;
+`;
+
+const Header = styled.h1`
+  font-family: Roboto;
+  font-size: 3em;
+`;
+
 const InputText = styled.div`
   display: flex;
   flex-direction: column;
+`;
+
+const InputLabel = styled.label`
+  font-family: Roboto;
+  font-size: 1em;
+  color: #555;
+  margin-bottom: 1em;
+`;
+
+const InputField = styled.input`
+  font-family: Roboto;
+  font-size: 2em;
+  border: none;
+  border-bottom: 2px solid #aaa;
+  padding-bottom: 0.2em;
+  margin-bottom: 2em;
+
+  ::placeholder {
+    color: #aaa;
+  }
+
+  :focus {
+    border: none;
+    outline: none;
+    border-bottom: 2px solid #000;
+  }
+`;
+
+const InputTextArea = styled.textarea`
+  font-family: Roboto;
+  font-size: 2em;
+  border: none;
+  border-bottom: 2px solid #aaa;
+  margin-bottom: 2em;
+  padding-bottom: 0.2em;
+  height: auto;
+
+  ::placeholder {
+    color: #aaa;
+  }
+
+  :focus {
+    border: none;
+    outline: none;
+    border-bottom: 2px solid #000;
+  }
+`;
+
+const Button = styled.button`
+  background-color: #000;
+  border: none;
+  color: white;
+  padding: 10px 10px;
+  margin-top: 10px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 1.2em;
+  font-family: Roboto;
 `;
 
 class Post extends React.Component {
@@ -26,22 +94,37 @@ class Post extends React.Component {
 
   render = () => {
     return (
-      <div>
-        <h1>Post an Experiment</h1>
+      <Wrapper>
+        <Header>Post an Experiment</Header>
         <InputText>
-          <label>title</label>
-          <input name="title" type="text" onChange={this.handleChange} />
+          <InputLabel>What's the name of your experiment?</InputLabel>
+          <InputField
+            name="title"
+            type="text"
+            onChange={this.handleChange}
+            placeholder="Give it something catchy!"
+          />
         </InputText>
         <InputText>
-          <label>description</label>
-          <textarea name="desc" type="text" onChange={this.handleChange} />
+          <InputLabel>What do you want participants to do?</InputLabel>
+          <InputTextArea
+            name="desc"
+            type="text"
+            onChange={this.handleChange}
+            placeholder="What's the goal, how long will they take, and what's in it for them?"
+          />
         </InputText>
         <InputText>
-          <label>url</label>
-          <input name="url" type="text" onChange={this.handleChange} />
+          <InputLabel>What's the URL of your experiment?</InputLabel>
+          <InputField
+            name="url"
+            type="text"
+            onChange={this.handleChange}
+            placeholder="https://..."
+          />
         </InputText>
-        <button onClick={this.handleClick}>Post</button>
-      </div>
+        <Button onClick={this.handleClick}>Post it!</Button>
+      </Wrapper>
     );
   };
 }
