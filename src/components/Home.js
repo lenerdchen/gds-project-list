f990dc8a12fc3ea1474317e60f6d608fe006d699
Leonard Chen <>
2018-09-03 19:43:41 +0800

    revert List to a styled.ul, put back keys to li tag

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,50 +1,59 @@
 import React from "react";
 import styled from "styled-components";
 
-const List = styled.div`
-  ul {
-    list-style-type: none;
-    font-family: Roboto, san-serif;
+const Wrapper = styled.div`
+  margin: 2em;
+`;
 
-    li {
-      padding-top: 10px;
+const Header = styled.h1`
+  font-size: 3em;
+  font-family: Roboto;
+`;
 
-      span.title {
-        font-size: 2em;
-        line-height: 2em;
-      }
+const List = styled.ul`
+  list-style: none;
+  font-family: Roboto;
+  margin-left: -40px;
 
-      span.desc {
-        font-size: 1.6em;
-        line-height: 1.6em;
-      }
+  li {
+    padding-top: 0.5em;
+    margin-bottom: 2em;
+
+    span.title {
+      font-size: 2em;
+      line-height: 2em;
     }
 
-    button {
-      background-color: #000;
-      border: none;
-      color: white;
-      padding: 10px 10px;
-      margin-top: 10px;
-      text-align: center;
-      text-decoration: none;
-      display: inline-block;
-      font-size: 1.2em;
-      font-family: inherit;
+    span.desc {
+      font-size: 1.6em;
+      line-height: 1.6em;
     }
+  }
+
+  button {
+    background-color: #000;
+    border: none;
+    color: white;
+    padding: 10px 10px;
+    margin-top: 10px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 1.2em;
+    font-family: inherit;
   }
 `;
 
 class Home extends React.Component {
   render = () => {
     return (
-      <div>
-        <h1>Home</h1>
-        <List>
+      <Wrapper>
+        <Header>Home</Header>
+        <div>
           {this.props.projects.map(project => {
             return (
-              <ul key={project.title}>
-                <li>
+              <List>
+                <li key={project.title}>
                   <a href={project.url}>
                     <span className="title">{project.title}</span>
                   </a>
@@ -53,11 +62,11 @@ class Home extends React.Component {
                   <br />
                   <button href={project.url}>Do it!</button>
                 </li>
-              </ul>
+              </List>
             );
           })}
-        </List>
-      </div>
+        </div>
+      </Wrapper>
     );
   };
 }
