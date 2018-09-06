2c578eb8157ebd52e8f94f1552ec80cae018ed2a
Leonard Chen <>
2018-09-06 22:43:32 +0800

    updated styles for home

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,35 +14,8 @@ const Header = styled.h1`
 const List = styled.ul`
   list-style: none;
   font-family: Roboto;
+  font-size: 2em;
   margin-left: -40px;
-
-  li {
-    padding-top: 0.5em;
-    margin-bottom: 2em;
-
-    span.title {
-      font-size: 2em;
-      line-height: 2em;
-    }
-
-    span.desc {
-      font-size: 1.6em;
-      line-height: 1.6em;
-    }
-  }
-
-  button {
-    background-color: #000;
-    border: none;
-    color: white;
-    padding: 10px 10px;
-    margin-top: 10px;
-    text-align: center;
-    text-decoration: none;
-    display: inline-block;
-    font-size: 1.2em;
-    font-family: inherit;
-  }
 `;
 
 class Home extends React.Component {
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,17 +1,44 @@
 import React from "react";
+import styled from "styled-components";
+
+const ListItem = styled.li`
+  padding-top: 0.5em;
+  margin-bottom: 2em;
+
+  span.title {
+    font-size: 1.2em;
+    margin-bottom: 1em;
+  }
+`;
+
+const Button = styled.button`
+  background-color: #000;
+  border: none;
+  color: white;
+  padding: 10px 10px;
+  margin-top: 10px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: inherit;
+  font-family: inherit;
+  cursor: pointer;
+`;
 
 class Project extends React.Component {
   render() {
     const { title, desc, url } = this.props.details;
 
     return (
-      <li>
-        <span>{title}</span>
+      <ListItem>
+        <span class="title">
+          <a href={title}>{title}</a>
+        </span>
         <br />
-        <span>{desc}</span>
+        <span class="desc">{desc}</span>
         <br />
-        <span>{url}</span>
-      </li>
+        <Button href={url}>Participate</Button>
+      </ListItem>
     );
   }
 }
